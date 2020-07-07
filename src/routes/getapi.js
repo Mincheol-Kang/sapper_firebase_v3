0b5b9c4b2e5a7876d58825565d10148290aa522d
Mincheol Kang <mindqcenter@naver.com>
2020-07-07 23:52:14 +0900

    불필요한 파일 제거

--- src/routes/getapi.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-export function get(res, req, next) {
-    fs.readFile("./multiply_.json", (err, data) => {
-        if (err) throw err;
-        let the_numbers = JSON.parse(data);
-        console.log(the_numbers);
-    });
-}
